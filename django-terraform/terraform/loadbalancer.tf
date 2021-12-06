ed1216145020975192969091083e8af5a4975612
paruliansaragi <parulian.saragi1@nhs.net>
2021-12-06 15:34:14 +0000

    ecs fargate django concrete example

---
+++ django-terraform/terraform/loadbalancer.tf
@@ -0,0 +1,52 @@
+resource "aws_lb" "production" {
+  name               = "${var.ecs_cluster_name}-alb"
+  load_balancer_type = "application"
+  security_groups    = [aws_security_group.load-balancer.id]
+  subnets = [
+    aws_subnet.public-subnet-1.id, aws_subnet.public-subnet-2.id
+  ]
+}
+
+resource "aws_alb_target_group" "default-target-group" {
+  name     = "${var.ecs_cluster_name}-default-tg"
+  port     = 80
+  protocol = "HTTP"
+  vpc_id   = aws_vpc.production-vpc.id
+  target_type = "ip"
+
+  health_check {
+    path                = "${var.health_check_path}"
+    port                = "traffic-port"
+    healthy_threshold   = 5
+    unhealthy_threshold = 2
+    timeout             = 20
+    interval            = 150
+    matcher             = "200-299"
+  }
+}
+
+# Listener (redirects traffic from the load balancer to the target group)
+resource "aws_alb_listener" "ecs-alb-http-listener" {
+  load_balancer_arn = aws_lb.production.id
+  port              = "80"
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_alb_target_group.default-target-group.arn
+  }
+}
+
+# resource "aws_alb_listener" "https" {
+#   load_balancer_arn = aws_lb.main.id
+#   port              = 443
+#   protocol          = "HTTPS"
+
+#   ssl_policy        = "ELBSecurityPolicy-2016-08"
+#   certificate_arn   = var.alb_tls_cert_arn
+
+#   default_action {
+#     target_group_arn = aws_alb_target_group.main.id
+#     type             = "forward"
+#   }
+# }
